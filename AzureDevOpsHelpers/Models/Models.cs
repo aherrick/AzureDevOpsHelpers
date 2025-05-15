19e5c5ca7fb7fc6ad1894c34281a5090eb5411ce
Andrew Herrick <aj@andrewherrick.com>
2025-05-15 14:33:53 -0400

    clean up

--- AzureDevOpsHelpers/Models/Models.cs
+++ AzureDevOpsHelpers/Models/Models.cs
@@ -1,5 +1,9 @@
-﻿namespace AzureDevOpsHelpers.Models;
+﻿using System.Text.Json;
+
+namespace AzureDevOpsHelpers.Models;
 
 public record User(string Id, string DisplayName, string Email);
 
 public record FileUnifiedDiff(string FilePath, string UnifiedDiff);
+
+public record PullRequestDto(JsonElement.ArrayEnumerator Changes, string RepositoryId, string BaseCommitId, string TargetCommitId)
--- AzureDevOpsHelpers/Services/DataService.cs
+++ AzureDevOpsHelpers/Services/DataService.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Web;
@@ -50,7 +51,7 @@ public class DataService(string org, string pat)
                 }
                 uriBuilder.Query = query.ToString();
 
-                var response = JsonDocument.Parse(await MakeApiCall(uriBuilder.ToString()));
+                var response = JsonDocument.Parse(await GET(uriBuilder.ToString()));
                 if (
                     response != null
                     && response.RootElement.TryGetProperty("members", out var members)
@@ -96,7 +97,7 @@ public class DataService(string org, string pat)
     {
         string url =
             $"https://vsaex.dev.azure.com/{org}/_apis/userentitlements/{userId}?api-version=5.1-preview.2";
-        var response = JsonDocument.Parse(await MakeApiCall(url));
+        var response = JsonDocument.Parse(await GET(url));
         List<string> projects = [];
 
         if (
@@ -115,14 +116,14 @@ public class DataService(string org, string pat)
         return projects;
     }
 
-    public async Task<List<FileUnifiedDiff>> GetUniffedDiffForPullRequest(
+    private async Task<PullRequestDto> GetPullRequestChanges(
         string project,
         string repoName,
         int pullRequestId
     )
     {
         var repoJsonDoc = JsonDocument.Parse(
-            await MakeApiCall(
+            await GET(
                 $"https://dev.azure.com/{org}/{project}/_apis/git/repositories/{repoName}?api-version=7.1-preview.1"
             )
         );
@@ -130,7 +131,7 @@ public class DataService(string org, string pat)
         var repoId = repoJsonDoc.RootElement.GetProperty("id").GetString();
 
         var prJsonDoc = JsonDocument.Parse(
-            await MakeApiCall(
+            await GET(
                 $"https://dev.azure.com/{org}/{project}/_apis/git/repositories/{repoId}/pullRequests/{pullRequestId}?api-version=7.1"
             )
         );
@@ -146,14 +147,188 @@ public class DataService(string org, string pat)
             .GetString();
 
         var changesJsonDoc = JsonDocument.Parse(
-            await MakeApiCall(
+            await GET(
                 $"https://dev.azure.com/{org}/{project}/_apis/git/repositories/{repoId}/diffs/commits?baseVersionType=commit&baseVersion={baseCommitId}&targetVersionType=commit&targetVersion={targetCommitId}&api-version=7.1-preview.1"
             )
         );
 
+        return new PullRequestDto(
+            changesJsonDoc.RootElement.GetProperty("changes").EnumerateArray(),
+            repoId,
+            baseCommitId,
+            targetCommitId
+        );
+    }
+
+    public async Task GetDiffForPullRequest(string project, string repoName, int pullRequestId)
+    {
+        var prDto = await GetPullRequestChanges(project, repoName, pullRequestId);
+
+        foreach (var change in prDto.Changes)
+        {
+            if (
+                change.TryGetProperty("item", out var item)
+                && item.TryGetProperty("path", out var path)
+                && item.TryGetProperty("gitObjectType", out var type)
+                && type.GetString() == "blob"
+            )
+            {
+                var filePath = path.GetString();
+
+                var fileDiffBuilder = new StringBuilder();
+
+                // Add filepath header
+                fileDiffBuilder.AppendLine($"src: {filePath}");
+                fileDiffBuilder.AppendLine();
+
+                var body = new
+                {
+                    contributionIds = new[] { "ms.vss-code-web.file-diff-data-provider" },
+                    dataProviderContext = new
+                    {
+                        properties = new
+                        {
+                            repositoryId = prDto.RepositoryId,
+                            diffParameters = new
+                            {
+                                includeCharDiffs = true,
+                                modifiedPath = filePath,
+                                modifiedVersion = $"GC{prDto.BaseCommitId}",
+                                originalPath = filePath,
+                                originalVersion = $"GC{prDto.TargetCommitId}",
+                                partialDiff = true,
+                                forceLoad = false,
+                            },
+                        },
+                    },
+                };
+
+                var bodySerialize = JsonSerializer.Serialize(body);
+
+                var result = await POST(
+                    $"https://dev.azure.com/{org}/_apis/Contribution/HierarchyQuery/project/{project}?api-version=5.0-preview.1",
+                    bodySerialize
+                );
+
+                using var jsonDoc = JsonDocument.Parse(result);
+                var blocks = jsonDoc
+                    .RootElement.GetProperty("dataProviders")
+                    .GetProperty("ms.vss-code-web.file-diff-data-provider")
+                    .GetProperty("blocks")
+                    .EnumerateArray()
+                    .ToList();
+
+                // Group blocks into sections based on truncation attributes
+                List<List<JsonElement>> sections = [];
+                List<JsonElement> currentSection = [];
+                bool? lastTruncatedAfter = null;
+
+                foreach (var block in blocks)
+                {
+                    bool truncatedBefore = false;
+                    bool truncatedAfter = false;
+
+                    if (block.TryGetProperty("truncatedBefore", out var truncatedBeforeProp))
+                    {
+                        truncatedBefore = truncatedBeforeProp.GetBoolean();
+                    }
+                    if (block.TryGetProperty("truncatedAfter", out var truncatedAfterProp))
+                    {
+                        truncatedAfter = truncatedAfterProp.GetBoolean();
+                    }
+
+                    if (lastTruncatedAfter == true && truncatedBefore)
+                    {
+                        sections.Add(currentSection);
+                        currentSection = [];
+                    }
+
+                    currentSection.Add(block);
+                    lastTruncatedAfter = truncatedAfter;
+                }
+                if (currentSection.Count > 0)
+                {
+                    sections.Add(currentSection);
+                }
+
+                // Process each section
+                int currentMLine = 0; // Running line number for modified file
+
+                for (int i = 0; i < sections.Count; i++)
+                {
+                    var section = sections[i];
+
+                    foreach (var block in section)
+                    {
+                        int changeType = block.GetProperty("changeType").GetInt32();
+                        var oLines = block
+                            .GetProperty("oLines")
+                            .EnumerateArray()
+                            .Select(e => e.GetString()!)
+                            .ToList();
+                        var mLines = block
+                            .GetProperty("mLines")
+                            .EnumerateArray()
+                            .Select(e => e.GetString()!)
+                            .ToList();
+                        int oLine = block.GetProperty("oLine").GetInt32();
+
+                        if (changeType == 0) // Unchanged
+                        {
+                            for (int j = 0; j < oLines.Count; j++)
+                            {
+                                diffBuilder.AppendLine($" {oLine + j,4} |    {oLines[j]}");
+                                currentMLine++;
+                            }
+                        }
+                        else if (changeType == 2) // Deleted
+                        {
+                            for (int j = 0; j < oLines.Count; j++)
+                            {
+                                diffBuilder.AppendLine($"-{oLine + j,4} |    {oLines[j]}");
+                            }
+                            // Do not increment currentMLine for deletions, as they don't appear in modified file
+                        }
+                        else if (changeType == 1) // Added
+                        {
+                            for (int j = 0; j < mLines.Count; j++)
+                            {
+                                diffBuilder.AppendLine($"+{currentMLine + j,4} |    {mLines[j]}");
+                            }
+                            currentMLine += mLines.Count;
+                        }
+                    }
+
+                    // Add separator between sections, but not after the last one
+                    if (i < sections.Count - 1)
+                    {
+                        diffBuilder.AppendLine("---");
+                        diffBuilder.AppendLine();
+                    }
+                }
+
+                // Add empty line after each file's diff
+                diffBuilder.AppendLine();
+            }
+        }
+
+        // Output the complete diff
+        Console.WriteLine(diffBuilder.ToString());
+
+        Console.ReadLine();
+    }
+
+    public async Task<List<FileUnifiedDiff>> GetUniffedDiffForPullRequest(
+        string project,
+        string repoName,
+        int pullRequestId
+    )
+    {
         var filleUnifiedDiffDtos = new List<FileUnifiedDiff>();
 
-        foreach (var change in changesJsonDoc.RootElement.GetProperty("changes").EnumerateArray())
+        var prDto = await GetPullRequestChanges(project, repoName, pullRequestId);
+
+        foreach (var change in prDto.Changes)
         {
             if (
                 change.TryGetProperty("item", out var item)
@@ -164,12 +339,12 @@ public class DataService(string org, string pat)
             {
                 var filePath = path.GetString()!;
 
-                var oldText = await MakeApiCall(
-                    $"https://dev.azure.com/{org}/{project}/_apis/git/repositories/{repoId}/items?path={Uri.EscapeDataString(filePath)}&versionDescriptor.version={baseCommitId}&versionDescriptor.versionType=commit&api-version=7.1"
+                var oldText = await GET(
+                    $"https://dev.azure.com/{org}/{project}/_apis/git/repositories/{prDto.RepositoryId}/items?path={Uri.EscapeDataString(filePath)}&versionDescriptor.version={prDto.BaseCommitId}&versionDescriptor.versionType=commit&api-version=7.1"
                 );
 
-                var newText = await MakeApiCall(
-                    $"https://dev.azure.com/{org}/{project}/_apis/git/repositories/{repoId}/items?path={Uri.EscapeDataString(filePath)}&versionDescriptor.version={targetCommitId}&versionDescriptor.versionType=commit&api-version=7.1"
+                var newText = await GET(
+                    $"https://dev.azure.com/{org}/{project}/_apis/git/repositories/{prDto.RepositoryId}/items?path={Uri.EscapeDataString(filePath)}&versionDescriptor.version={prDto.TargetCommitId}&versionDescriptor.versionType=commit&api-version=7.1"
                 );
 
                 var diff = InlineDiffBuilder.Diff(oldText, newText);
@@ -222,7 +397,17 @@ public class DataService(string org, string pat)
         return filleUnifiedDiffDtos;
     }
 
-    private async Task<string> MakeApiCall(string url, bool parseJson = true)
+    private async Task<string> GET(string url)
+    {
+        return await MakeApiCall(url, HttpMethod.Get, null);
+    }
+
+    private async Task<string> POST(string url, string content = null)
+    {
+        return await MakeApiCall(url, HttpMethod.Post, content);
+    }
+
+    private async Task<string> MakeApiCall(string url, HttpMethod method, string content)
     {
         using var client = new HttpClient();
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
@@ -230,7 +415,20 @@ public class DataService(string org, string pat)
             Convert.ToBase64String(Encoding.ASCII.GetBytes($":{pat}"))
         );
 
-        HttpResponseMessage response = await client.GetAsync(url);
+        HttpResponseMessage response;
+        if (method == HttpMethod.Post)
+        {
+            var httpContent =
+                content != null
+                    ? new StringContent(content, Encoding.UTF8, "application/json")
+                    : null;
+            response = await client.PostAsync(url, httpContent);
+        }
+        else
+        {
+            response = await client.GetAsync(url);
+        }
+
         if (!response.IsSuccessStatusCode)
         {
             Console.WriteLine(
